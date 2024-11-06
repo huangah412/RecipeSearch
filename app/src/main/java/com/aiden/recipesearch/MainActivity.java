c5091c5d41048f9ab7db3931083a97f85cf5f17f
aiden <huangah412@gmail.com>
2024-11-06 10:14:41 -0500

    BottomNavigationView created Fragments to switch between home and ingredients

--- app/src/main/java/com/aiden/recipesearch/MainActivity.java
+++ app/src/main/java/com/aiden/recipesearch/MainActivity.java
@@ -1,12 +1,14 @@
 package com.aiden.recipesearch;
 
 import android.os.Bundle;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +22,23 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        NavigationBarView navigation = findViewById(R.id.navigation);
+        navigation.setOnItemSelectedListener(item -> {
+            int id = item.getItemId();
+            Fragment selectedFragment = null;
+            if (id == R.id.home){
+                selectedFragment = new HomeFragment();
+            } else if (id == R.id.ingredients){
+                selectedFragment = new IngredientsFragment();
+            }
+
+            if(selectedFragment != null){
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+            }
+            return true;
+        });
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
     }
 }
